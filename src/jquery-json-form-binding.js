ddfc6ab35a572da8c225342c50590e2ccbfc666c
sesubash <sesubash@gmail.com>
2020-06-14 19:01:59 +0530

    refactored the code to make it work for multiple select, radio, checkbox group

--- src/jquery-json-form-binding.js
+++ src/jquery-json-form-binding.js
@@ -18,29 +18,29 @@ $.fn.jsonToForm = function (data, callbacks) {
 
                 options.callbacks[k](v);
 
-            } else if(elements.length || v.length) {
+            } else if (elements.length || v.length) {
                 // check number of occurance of the element, 
                 // radio group / checkbox element will be having same name and hence multiple occurance will be there inside a form
                 $(elements).each(function (index, ele) {
-                    if (Array.isArray(v)) {
-                      v.forEach(function (val) {
-                        $(ele).is("select") // check if select control
-                          ? $(ele)
-                              .find("[value='" + val + "']")
-                              .prop("selected", true) // select option if select control
-                          : $(ele).val() == val
-                          ? $(ele).prop("checked", true) // mark as checked if radio or checkbox
-                          : "";
-                      });
+                    if (Array.isArray(v)) { // checkbox group and multiple select will have array of values
+                        v.forEach(function (val) {
+                            $(ele).is("select") // check if select control
+                                ? $(ele)
+                                    .find("[value='" + val + "']")
+                                    .prop("selected", true) // select option if select control
+                                : ($(ele).val() == val)
+                                    ? $(ele).prop("checked", true) // mark as checked if radio or checkbox
+                                    : "";
+                        });
+                    } else if ($(ele).is(':checkbox') || $(ele).is(':radio')) {  // checkbox group or radio group
+                        ($(ele).val() == v) ? $(ele).prop("checked", true) : "";
                     } else {
-                      $(ele).val() == v ? $(ele).prop("checked", true) : "";
+                        $('[name="' + k + '"]', formInstance).val(v);
                     }
                 });
            
                     
-            } else {
-                    $('[name="' + k + '"]', formInstance).val(v);
-                }
+            }
 
         });
     }
