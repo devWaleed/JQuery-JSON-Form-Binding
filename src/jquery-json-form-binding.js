f872f11279ac9a2902cffbd513e108e1dcdd5503
Waleed <devwaleed@gmail.com>
2021-04-05 01:17:31 +0500

    added example folder and improved code

--- src/jquery-json-form-binding.js
+++ src/jquery-json-form-binding.js
@@ -1,48 +1,38 @@
 $.fn.jsonToForm = function (data, callbacks) {
+  var formInstance = this;
 
-    var formInstance = this;
+  var options = {
+    data: data || null,
+    callbacks: callbacks,
+  };
 
-    var options = {
+  if (options.data != null) {
+    $.each(options.data, function (k, v) {
+      var elements = $('[name^="' + k + '"]', formInstance);
 
-        data: data || null,
-        callbacks: callbacks
+      if (options?.callbacks?.hasOwnProperty(k)) {
+        options.callbacks[k](v);
+        return;
+      }
 
-    };
-
-     if (options.data != null) {
-        $.each(options.data, function (k, v) {
-
-            var elements = $('[name="' + k + '"]', formInstance); 
-            
-            if (options.callbacks != null && options.callbacks.hasOwnProperty(k)) {
-
-                options.callbacks[k](v);
-
-            } else if (elements.length || v.length) {
-                // check number of occurance of the element, 
-                // radio group / checkbox element will be having same name and hence multiple occurance will be there inside a form
-                $(elements).each(function (index, ele) {
-                    if (Array.isArray(v)) { // checkbox group and multiple select will have array of values
-                        v.forEach(function (val) {
-                            $(ele).is("select") // check if select control
-                                ? $(ele)
-                                    .find("[value='" + val + "']")
-                                    .prop("selected", true) // select option if select control
-                                : ($(ele).val() == val)
-                                    ? $(ele).prop("checked", true) // mark as checked if radio or checkbox
-                                    : "";
-                        });
-                    } else if ($(ele).is(':checkbox') || $(ele).is(':radio')) {  // checkbox group or radio group
-                        ($(ele).val() == v) ? $(ele).prop("checked", true) : "";
-                    } else {
-                        $('[name="' + k + '"]', formInstance).val(v);
-                    }
-                });
-           
-                    
-            }
-
-        });
-    }
-
-}
+      $(elements).each(function (index, element) {
+        if (Array.isArray(v)) {
+          v.forEach(function (val) {
+            $(element).is("select")
+              ? $(element)
+                  .find("[value='" + val + "']")
+                  .prop("selected", true)
+              : $(element).val() == val
+              ? $(element).prop("checked", true)
+              : "";
+          });
+        } else if ($(element).is(":checkbox") || $(element).is(":radio")) {
+          // checkbox group or radio group
+          $(element).val() == v ? $(element).prop("checked", true) : "";
+        } else {
+          $('[name="' + k + '"]', formInstance).val(v);
+        }
+      });
+    });
+  }
+};
